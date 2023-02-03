ff1c409358b4a5faa0a4366179e534dfa67f5389
Vincenza Paradiso <122514035+vinniparadiso@users.noreply.github.com>
2023-02-03 11:42:55 -0500

    Add files via upload

---
+++ Quiz2.py
@@ -0,0 +1,12 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Vincenza Paradiso
+#5
+str(42)
+'42'
+#6
+int("Sunshine")
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    int("Sunshine")
+ValueError: invalid literal for int() with base 10: 'Sunshine'
